d1161ba231bebf2531f86724989d3f79cf5d4d8d
Kyara Massière <kyara.a.massiere@outlook.pt>
2026-01-28 09:52:29 -0300

    commit exercicios

---
+++ Exercicios/AULA10_EXERCICIO_IMC.py
@@ -0,0 +1,13 @@
+"""nome = 'Ana Maria'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+print(imc)"""
+
+#resultado deu certo 29.3209876...
+
+nome = "Kyara"
+altura = 1.67
+peso = 84
+imc = peso / altura **2
+print(nome," tem ", altura, "m de altura,pesa ",peso, "kg, e seu imc é de ", imc)
