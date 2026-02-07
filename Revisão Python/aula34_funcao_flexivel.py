e25ff0eebaa949219b231c204661a3d69145f4be
Kyara Massière <kyara.a.massiere@outlook.pt>
2026-02-07 10:49:47 -0300

    Finalização da revisão do python

---
+++ Revisão Python/aula34_funcao_flexivel.py
@@ -0,0 +1,6 @@
+def dobrar(numero):
+    resultado = numero * 2
+    print(f'O dobro do numero {numero} é {resultado}')
+dobrar(10)
+dobrar(20)
+dobrar(30)
---
+++ Revisão Python/aula35_funcao_flexivel2.py
@@ -0,0 +1,6 @@
+def somar(numero1, numero2):
+    resultado = numero1 + numero2
+    print(f'A soma de {numero1} com {numero2} é igual a {resultado}')
+
+somar(3,5)
+somar(20,14)
